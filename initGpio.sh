9ba53790ac8a831d676b55a8bcdd9d8b36d4fce3
Søren Andersen  (emb.) <s123369@stuent.dtu.dk>
2000-01-01 11:33:16 +1030

    first working example

--- initGpio.sh
+++ initGpio.sh
@@ -1,11 +1,12 @@
 # unload modules (reset)
 modprobe -r spidev
-modprobe -r spi-s3c64x
+modprobe -r spi-s3c64xx
 # load modules for spi
 modprobe spi-s3c64xx
 modprobe spidev
 
 # export IRQ gpio
 echo 199 > /sys/class/gpio/export
-
+echo in > /sys/class/gpio/gpio199/direction
+echo rising > /sys/class/gpio/gpio199/edge
 
