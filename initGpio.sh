cde8e2f75d27e4333148a191f9110754253688b7
Søren Andersen  (emb.) <s123369@stuent.dtu.dk>
2000-01-01 11:55:43 +1030

    fixed init script

--- initGpio.sh
+++ initGpio.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+# must be root
+# run with: su -c "sh initGpio.sh"
+
 # unload modules (reset)
 modprobe -r spidev
 modprobe -r spi-s3c64xx
@@ -5,8 +9,9 @@ modprobe -r spi-s3c64xx
 modprobe spi-s3c64xx
 modprobe spidev
 
-# export IRQ gpio
-echo 199 > /sys/class/gpio/export
+# export IRQ gpio, only if not yet exported
+[ -d "/sys/class/gpio/gpio199" ]&& echo "already exported" || echo 199 > /sys/class/gpio/export
+# set as input
 echo in > /sys/class/gpio/gpio199/direction
+# have to be superuser for this one
 echo rising > /sys/class/gpio/gpio199/edge
-
