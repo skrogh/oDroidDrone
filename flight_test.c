1a30a9a428b379520d2f452ea2481d6c78936c90
Skrogh <s123369@student.dtu.dk>
2015-03-02 17:42:11 +0100

    encoding fixed?

--- flight_test.c
+++ flight_test.c
@@ -37,22 +37,18 @@ void gpioIntHandler( void ) {
 	if ( ret < 1 )
 		perror( "can't send spi message" );
 
-	float acc[3] = (float*){
-		tr[3], tr[2], tr[1], tr[0],
-		tr[7], tr[6], tr[5], tr[4],
-		tr[11], tr[10], tr[9], tr[8],
-	}
-
 	printf("Acc: %3.3f\n     %3.3f\n     %3.3f\n",
-		acc[0], acc[1], acc[2] );
+		((float*)rx)[0],
+		((float*)rx)[1],
+		((float*)rx)[2] );
 	printf("Gyro: %3.3f\n      %3.3f\n      %3.3f\n",
-		((float*)tr)[3],
-		((float*)tr)[4],
-		((float*)tr)[5] );
+		((float*)rx)[3],
+		((float*)rx)[4],
+		((float*)rx)[5] );
 	printf("Alpha: %3.3f\n       %3.3f\n       %3.3f\n",
-		((float*)tr)[6],
-		((float*)tr)[7],
-		((float*)tr)[8] );
+		((float*)rx)[6],
+		((float*)rx)[7],
+		((float*)rx)[8] );
 /*
 	for ( ret = 0; ret < MESSAGE_LENGTH; ret++ ) {
 		if ( !( ret % 6 ) )
