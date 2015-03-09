122520d2c61525047ddf20c7b5c1140dda020dc8
Skrogh <s123369@student.dtu.dk>
2015-03-09 14:21:46 +0100

    restructure folders

--- clearLog.sh
+++ clearLog.sh
@@ -3,9 +3,9 @@
 echo "Clearing log files"
 #Clear images
 echo "Removing images:"
-[ "$(ls -A opencv_test_images)" ] && rm opencv_test_images/* || echo "No images to clear"
+[ "$(ls -A opencvImageGrab/opencv_test_images)" ] && rm opencvImageGrab/pencv_test_images/* || echo "No images to clear"
 
 #Clear log
 echo "Clearing log"
-[ -e log.csv ] && rm log.csv || echo "No log to clear"
+[ -e imuGrab/log.csv ] && rm imuGrab/log.csv || echo "No log to clear"
 
