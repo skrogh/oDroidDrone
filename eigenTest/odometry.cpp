94c184ee0a57182ecbcf837b1f23849806c7961a
Skrogh <s123369@student.dtu.dk>
2015-03-23 16:00:44 +0100

    minor fix

--- eigenTest/odometry.cpp
+++ eigenTest/odometry.cpp
@@ -203,7 +203,7 @@ void MSCKF::propagate( double a_m[3], double g_m[3] ) {
 
 	Matrix<double,15,15> Phi_I;
 	{
-		using Eigen::Matrix3d; // I'm lazy
+		using namespace Eigen::Matrix3d; // I'm lazy
 		Phi_I <<
 				Phi_qq,     Zero(),                      Zero(),    Phi_qgb,    Phi_qba,
 				Phi_pq, Identity(), calib->delta_t * Identity(),    Phi_pbg,    Phi_pba,
