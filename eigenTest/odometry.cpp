200843198926bf208226fcabe810c295f4bfc986
Skrogh <s123369@student.dtu.dk>
2015-03-23 16:30:24 +0100

    minor fix

--- eigenTest/odometry.cpp
+++ eigenTest/odometry.cpp
@@ -222,10 +222,10 @@ void MSCKF::propagate( double a_m[3], double g_m[3] ) {
 	Q_d = calib->delta_t * Phi_I * N_c * Phi_I.transpose() + N_c;
 
 	// do the update
-	std::cout << "225" << std::endl;
 	sigma.block<15,15>(0,0) = Phi_I * sigma.block<15,15>(0,0) * Phi_I.transpose() + Q_d;
-	std::cout << "227" << sigma.cols()-15 << "  " << sigma.cols() <<std::endl;
+
+	std:cout << Phi_I << "\n*" << sigma.block(15,sigma.cols()-15,0,15) << std:endl;
+
 	sigma.block(15,sigma.cols()-15,0,15) = Phi_I * sigma.block(15,sigma.cols()-15,0,15);
-	std::cout << "229" << std::endl;
 	sigma.block(sigma.rows()-15,15,15,0) = sigma.block(15,sigma.cols()-15,0,15).transpose();
 }
