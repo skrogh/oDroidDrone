cac88225bf4aaa9fac6a8e09e105a00f348774d4
Skrogh <s123369@student.dtu.dk>
2015-03-23 16:36:25 +0100

    minor fix

--- eigenTest/odometry.cpp
+++ eigenTest/odometry.cpp
@@ -223,9 +223,6 @@ void MSCKF::propagate( double a_m[3], double g_m[3] ) {
 
 	// do the update
 	sigma.block<15,15>(0,0) = Phi_I * sigma.block<15,15>(0,0) * Phi_I.transpose() + Q_d;
-
-	std::cout << Phi_I << "\n*" << sigma.block(0,15,15,sigma.cols()-15) << std::endl;
-
-	//sigma.block(15,sigma.cols()-15,0,15) = Phi_I * sigma.block(15,sigma.cols()-15,0,15);
-	//sigma.block(sigma.rows()-15,15,15,0) = sigma.block(15,sigma.cols()-15,0,15).transpose();
+	sigma.block(0,15,15,sigma.cols()-15) = Phi_I * sigma.block(0,15,15,sigma.cols()-15);
+	sigma.block(15,0,sigma.rows()-15,15) = sigma.block(0,15,15,sigma.cols()-15).transpose();
 }
