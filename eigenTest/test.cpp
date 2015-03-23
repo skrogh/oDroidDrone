78c498e92de8cf03cd1dc08e82e39af1650a5632
Skrogh <s123369@student.dtu.dk>
2015-03-23 10:44:02 +0100

    minor fix

--- eigenTest/test.cpp
+++ eigenTest/test.cpp
@@ -4,8 +4,8 @@ using namespace Eigen;
 
 Matrix3d crossMat( const Vector3d& v ){
 	Matrix3f m;
-	m <<     0, -v(2),  v(1)
-	      v(2),     0, -v(0)
+	m <<     0, -v(2),  v(1),
+	      v(2),     0, -v(0),
 	     -v(1),  v(0),     0;
 	return m;
 }
