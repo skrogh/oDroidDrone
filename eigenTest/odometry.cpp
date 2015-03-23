d32889a0c5cbea0e9ce09d7b7a415c9eb6292853
Skrogh <s123369@student.dtu.dk>
2015-03-23 10:40:39 +0100

    test for cross matrix

--- eigenTest/odometry.cpp
+++ eigenTest/odometry.cpp
@@ -1,4 +1,10 @@
-
+Matrix3d crossMat( const Vector3d& v ){
+	Matrix3f m;
+	m <<     0, -v(2),  v(1)
+	      v(2),     0, -v(0)
+	     -v(1),  v(0),     0;
+	return m;
+}
 
 
 void MSCKF::propagateState( double a_m[3], double g_m[3] ) {
--- eigenTest/test.cpp
+++ eigenTest/test.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include <Eigen/Dense>
 using namespace Eigen;
+
+Matrix3d crossMat( const Vector3d& v ){
+	Matrix3f m;
+	m <<     0, -v(2),  v(1)
+	      v(2),     0, -v(0)
+	     -v(1),  v(0),     0;
+	return m;
+}
+
 int main()
 {
   MatrixXd m(2,2);
@@ -11,5 +20,6 @@ int main()
   std::cout << m << std::endl;
 
   Vector3d v(1.0, 2.0, 3.0);
-  std::cout << v << " " << v.adjoint() << std::endl;
+  std::cout << v << " -> " << v.adjoint() << std::endl;
+  std::cout << v << " -> " << crossMat(v) << std::endl;
 }
