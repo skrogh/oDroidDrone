9d1d78fd3f48d49e9ac934792b87e5e3b3abad70
Skrogh <s123369@student.dtu.dk>
2015-03-24 19:05:32 +0100

    minor fix

--- eigenTest/test.cpp
+++ eigenTest/test.cpp
@@ -51,6 +51,9 @@ int main()
 	std::cout << "Sigma is " << msckf.sigma.rows() << "x" << msckf.sigma.cols() << " :\n" << msckf.sigma << std::endl;
 	std::cout << "State is " << msckf.x.rows() << "x" << msckf.x.cols() << " :\n" << msckf.x << std::endl;
 	msckf.augmentState();
+		for( int i = 0; i < 400; i++ ) {
+		msckf.propagate( a_m, g_m );
+	}
 	std::cout << "Sigma is " << msckf.sigma.rows() << "x" << msckf.sigma.cols() << " :\n" << msckf.sigma << std::endl;
 	std::cout << "State is " << msckf.x.rows() << "x" << msckf.x.cols() << " :\n" << msckf.x << std::endl;
 	msckf.removeOldStates( 1 );
