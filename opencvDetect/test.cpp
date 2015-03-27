74ef6f7cf13382bd2d9c6da5eb2c5c29e48eef21
Skrogh <s123369@student.dtu.dk>
2015-03-27 14:09:27 +0100

    minor fix

--- opencvDetect/test.cpp
+++ opencvDetect/test.cpp
@@ -84,7 +84,7 @@ int main( int argc, char** argv )
 	extractor->compute( img_2, keypoints_2, descriptors_2 );
 
 	//-- Step 3: Matching descriptor vectors with a brute force matcher
-	BFMatcher matcher(NORM_L2);
+	BFMatcher matcher(NORM_HAMMING);
 	std::vector< DMatch > matches;
 	matcher.match( descriptors_1, descriptors_2, matches );
 
