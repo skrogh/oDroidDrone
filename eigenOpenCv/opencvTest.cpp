9dd6f6ad79616dded003a5533e755fcfce66677e
Skrogh <s123369@student.dtu.dk>
2015-03-31 17:09:50 +0200

    minor fix

--- eigenOpenCv/opencvTest.cpp
+++ eigenOpenCv/opencvTest.cpp
@@ -29,7 +29,13 @@ int main( int argc, char** argv )
 		cameraFeatures.detectFeatures( image, meas );
 
 		// Iterate over meas and draw all non lost elements:
-
+		for ( std::list<CameraMeas_t>::iterator meas_j = meas.begin(); meas_j != meas.end(); ++meas_j ) {
+			if ( !meas_j->isLost ) {
+				Eigen::MatrixXd& z = meas_j->z;
+				cv::Point pt( z( z.rows()-1, 0 ), z( z.rows()-1, 0 ) );
+				cv::circle( image, pt, 4, Scalar( 255, 0, 0 ) );
+			}
+		}
 
 		cv::imshow("Matches", image );
 	}
