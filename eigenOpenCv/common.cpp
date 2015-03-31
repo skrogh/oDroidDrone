b4aa17ce2b2f182aef9be97dac0e4a466b66b699
Skrogh <s123369@student.dtu.dk>
2015-03-31 19:00:17 +0200

    minor fix

--- eigenOpenCv/common.cpp
+++ eigenOpenCv/common.cpp
@@ -7,7 +7,7 @@
 
 void CameraMeasurements::addFeatures( const std::vector<cv::KeyPoint> &keypointsOld, const std::vector<cv::KeyPoint> &keypointsNew,
 		const std::vector< cv::DMatch >& matches ) {
-	linkOld = link;
+	std::vector< std::list<CameraMeas_t>::iterator > linkOld = link;
 
 
 	// Go through all measurements and mark all of them as lost
@@ -41,9 +41,9 @@ void CameraMeasurements::addFeatures( const std::vector<cv::KeyPoint> &keypoints
 			// Add a new entry and link it
 			link[matches[i].trainIdx] = this->addFeature();
 			// Add data to it
-			addToFeature( linkNew[matches[i].trainIdx],
+			addToFeature( link[matches[i].trainIdx],
 					keypointsOld[matches[i].queryIdx].pt.x, keypointsOld[matches[i].queryIdx].pt.y );
-			addToFeature( linkNew[matches[i].trainIdx],
+			addToFeature( link[matches[i].trainIdx],
 					keypointsNew[matches[i].trainIdx].pt.x, keypointsNew[matches[i].trainIdx].pt.y );
 			// Add link back
 			this->linkBack( link[matches[i].trainIdx], matches[i].trainIdx );
@@ -53,13 +53,13 @@ void CameraMeasurements::addFeatures( const std::vector<cv::KeyPoint> &keypoints
 
 
 void CameraMeasurements::linkBack( std::list<CameraMeas_t>::iterator& feature, int linkLink ) {
-	feature->linkLink = linkLink
+	feature->linkLink = linkLink;
 }
 
 void CameraMeasurements::addToFeature( std::list<CameraMeas_t>::iterator& feature,
 		double x, double y ) {
 	// Add feature TODO: consider if theis matrix should be made a vector
-	MatrixX2d& z = feature->z;
+	Eignen::MatrixX2d& z = feature->z;
 	z.conservativeResize ( z.rows() + 1, Eignen::NoChange );
 	z.block<1,2>( z.rows()-1, 0 ) <<
 			x,
@@ -70,8 +70,9 @@ void CameraMeasurements::addToFeature( std::list<CameraMeas_t>::iterator& featur
 
 std::list<CameraMeas_t>::iterator CameraMeasurements::addFeature( ) {
 	// Add a new entry
-	CameraMeas_t z =Eigen::MatrixX2d( 0, 2 );
+	CameraMeas_t z;
 	std::list<CameraMeas_t>::iterator newFeature = meas.insert( meas.begin(), z );
+	newFeature->z = Eigen::MatrixX2d( 0, 2 );
 	// lost (well empty)
 	newFeature->isLost = true;
 	// no link back to link yet
