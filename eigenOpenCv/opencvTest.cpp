e5dd3d3d6f7de8164c33e90ef747067c3faaffe6
Skrogh <s123369@student.dtu.dk>
2015-03-31 17:14:57 +0200

    minor fix

--- eigenOpenCv/opencvTest.cpp
+++ eigenOpenCv/opencvTest.cpp
@@ -32,7 +32,7 @@ int main( int argc, char** argv )
 		for ( std::list<CameraMeas_t>::iterator meas_j = meas.begin(); meas_j != meas.end(); ++meas_j ) {
 			if ( !meas_j->isLost ) {
 				Eigen::MatrixX2d& z = meas_j->z;
-				cv::Point pt = Point( z( z.rows()-1, 0 ), z( z.rows()-1, 0 ) );
+				cv::Point pt = Point( z( z.rows()-1, 0 ), z( z.rows()-1, 1 ) );
 				cv::circle( image, pt, 4, Scalar( 255, 0, 0 ) );
 			}
 		}
