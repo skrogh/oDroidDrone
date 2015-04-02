43220f1bdb1538ffeb731f52f8f57bcd09f24976
Skrogh <s123369@student.dtu.dk>
2015-04-02 17:29:48 +0200

    minor fix

--- eigenOpenCv/imuTest.cpp
+++ eigenOpenCv/imuTest.cpp
@@ -12,6 +12,18 @@ int main( int argc, char** argv )
 	std::cout << "Press a key..." << std::endl;
 	std::cin >> wait;
 	std::cout << "Got: " << imu.fifoSize() << " Imu samples" << std::endl;
+	ImuMeas_t element;
+	while( fifoPop( element ) ) {
+		std::cout <<
+		"Acc: " <<
+		element.acc[0] << ", " << element.acc[1] << ", " <<  element.acc[2] << "\n" << 
+		"Gyro: " <<
+		element.gyro[0] << ", " << element.gyro[1] << ", " <<  element.gyro[2] << "\n" << 
+		"Alpha: " <<
+		element.alpha[0] << ", " << element.alpha[1] << ", " <<  element.alpha[2] << "\n" << 
+		"Dist: " <<
+		element.dist << "\n" << std::endl;
+	}
 	std::cout << "Progam ended" << std::endl;
 	return 0;
 }
