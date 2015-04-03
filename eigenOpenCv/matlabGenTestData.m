1cafc613d3de8622561de7ccb66c0fd2c17178d8
Skrogh <s123369@student.dtu.dk>
2015-04-03 18:14:01 +0200

    r0 H0 not dynamic

---
+++ eigenOpenCv/matlabGenTestData.m
@@ -0,0 +1,48 @@
+%% set calib file
+calib.o_x = 300.8859;
+calib.o_y = 222.5206;
+calib.f_x = 411.1170;
+calib.f_y = 409.9516;
+calib.k1 = -0.3453;
+calib.k2 = 0.1012;   
+calib.k3 = 0;
+calib.t1 = -0.0003;
+calib.t2 = 0.0014;
+
+calib.CI_q = axisAngle2quatern( [0,1,-1], pi)'; % rotation from inertial frame to camera
+calib.CI_q = quaternProd( calib.CI_q',...
+    axisAngle2quatern( [1,0,0], -pi/4)...
+     )';
+calib.C_p_I = [0, 0.0, -0.056]';%position of camera in inertial frame
+
+calib.g = 9.82;
+calib.Delta_t = 1/400;
+calib.sigma_gd = 0.01;
+calib.sigma_ad = 0.01;
+calib.sigma_wgd = 0.05 * sqrt( calib.Delta_t );
+calib.sigma_wad = 0.1 * sqrt( calib.Delta_t );
+
+calib.sigma_gc = calib.sigma_gd * sqrt( calib.Delta_t );
+calib.sigma_ac = calib.sigma_ad * sqrt( calib.Delta_t );
+calib.sigma_wgc = calib.sigma_wgd / sqrt( calib.Delta_t );
+calib.sigma_wac = calib.sigma_wad / sqrt( calib.Delta_t );
+
+calib.sigma_dc = 0.05;
+
+calib.sigma_Im = 40;
+
+calib.crop_x1 = 0;
+calib.crop_x2 = 640;
+calib.crop_y1 = 0;
+calib.crop_y2 = 480;
+
+calib.maxFrame = 5;
+
+calib.imageOffset = 0.033; %delay of camera with respect to imu in s
+
+
+z = [0;0];
+x_Ci = [ [ 0, 1, 0, 0 ] [ 0, 0, 1 ] [ 0, 0, 0 ] ]';
+
+
+[ G_p_f, f, J_h, theta_log, G_p_f_log ] = triangulate( z, x_Ci, calib )
---
+++ eigenOpenCv/matlabUnitTest.cpp
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sys/time.h>
+
+#include "feature.hpp"
+#include "odometry.hpp"
+#include "common.hpp"
+#include "imu.hpp"
+
+int main( int argc, char** argv )
+{
+	Calib calib;
+	calib.o_x = 300.8859;
+	calib.o_y = 222.5206;
+	calib.f_x = 411.1170;
+	calib.f_y = 409.9516;
+	calib.k1 = -0.3453;
+	calib.k2 = 0.1012;
+	calib.t1 = -0.0003;
+	calib.t2 = 0.0014;
+	calib.CI_q = Eigen::Quaternion<double>(
+			0.000000000000000, // note that the real value comes first in the eigen quaterneon constructor
+			-0.000000000000000,
+			0.382683432365090,
+			-0.923879532511287
+	);
+	calib.C_p_I = Eigen::Vector3d( 0.0, 0.0, -0.056 );
+	calib.g = 9.82;
+	calib.delta_t = 0.0025;
+	calib.imageOffset.tv_sec = 0;
+	calib.imageOffset.tv_usec = 33000;
+	calib.sigma_gc = 0.001;//5.0e-04;
+	calib.sigma_ac = 0.008;//5.0e-04;
+	calib.sigma_wgc = 0.0001;
+	calib.sigma_wac = 0.0001;
+	calib.sigma_Im = 40;
+	calib.sigma_hc = 0.05;
+	calib.minFrame = 1;
+	std::cout << "calib is:\n" << calib << std::endl;
+
+
+	MSCKF msckf( &calib );
+	// initialize upside down
+	msckf.x.block<4,1>(0,0) << 0,1,0,0;
+
+	// Test triangulate
+	// set position and push some frames:
+	msckf.x.block<3,1>(4,0) << 0,0,1;
+	msckf.augmentState();
+	msckf.x.block<3,1>(4,0) << 0,0,0;
+	msckf.augmentState();
+	msckf.x.block<3,1>(4,0) << 0,0,0;
+	Egien::MatrixX2d z( 2, 1 );
+	z << 0,0;
+	std::cout << "Triangulate test1: " << msckf.triangulate( z ) << std::endl;
+
+
+
+}
