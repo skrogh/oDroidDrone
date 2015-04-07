a4cbc05e18932a49d189268ccf7f4376a971c382
Skrogh <s123369@student.dtu.dk>
2015-04-07 16:52:06 +0200

    added alias quaternion def.

--- eigenOpenCv/quaternionAlias.hpp
+++ eigenOpenCv/quaternionAlias.hpp
@@ -57,7 +57,7 @@ public:
 	{ m_coeffs = other.coeffs().template cast<Scalar>(); }
 */
 
-	template<class OtherDerived> EIGEN_STRONG_INLINE QuaternionAlias<Scalar> operator* (const QuaternionAlias<OtherDerived>& q) const
+	template<class OtherDerived> EIGEN_STRONG_INLINE QuaternionAlias<Scalar> operator* (const QuaternionAlias<OtherDerived>& other) const
 	{
 	EIGEN_STATIC_ASSERT((internal::is_same<typename This::Scalar, typename OtherDerived::Scalar>::value),
 		YOU_MIXED_DIFFERENT_NUMERIC_TYPES__YOU_NEED_TO_USE_THE_CAST_METHOD_OF_MATRIXBASE_TO_CAST_NUMERIC_TYPES_EXPLICITLY)
