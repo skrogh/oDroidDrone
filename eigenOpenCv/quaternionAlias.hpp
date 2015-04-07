840eb58d4ee6917f99afc110c628fd41dae483bb
Skrogh <s123369@student.dtu.dk>
2015-04-07 17:11:17 +0200

    added alias quaternion def.

--- eigenOpenCv/quaternionAlias.hpp
+++ eigenOpenCv/quaternionAlias.hpp
@@ -57,13 +57,13 @@ public:
 	{ m_coeffs = other.coeffs().template cast<Scalar>(); }
 */
 
-	template<class OtherDerived> EIGEN_STRONG_INLINE QuaternionAlias<Scalar> operator* (const QuaternionBase<OtherDerived>& other) const
+	template<class OtherDerived> EIGEN_STRONG_INLINE QuaternionAlias<_Scalar, _Options> operator* (const QuaternionBase<OtherDerived>& other) const
 	{
 	EIGEN_STATIC_ASSERT((internal::is_same<typename QuaternionAlias<_Scalar,_Options>::Scalar, typename OtherDerived::Scalar>::value),
 		YOU_MIXED_DIFFERENT_NUMERIC_TYPES__YOU_NEED_TO_USE_THE_CAST_METHOD_OF_MATRIXBASE_TO_CAST_NUMERIC_TYPES_EXPLICITLY)
 	return internal::quat_product< Architecture::Target, QuaternionAlias< _Scalar,_Options>, OtherDerived,
 			typename internal::traits<QuaternionAlias<_Scalar,_Options> >::Scalar,
-			internal::traits<Quaternion<_Scalar,_Options> >::IsAligned && internal::traits<OtherDerived>::IsAligned >::run(*this, other);
+			internal::traits<QuaternionAlias<_Scalar,_Options> >::IsAligned && internal::traits<OtherDerived>::IsAligned >::run(*this, other);
 	}
 
 	
