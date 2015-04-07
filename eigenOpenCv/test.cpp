3c3fc27aa548dec0e79aea3653cbef2328dac4a4
Skrogh <s123369@student.dtu.dk>
2015-04-07 17:42:34 +0200

    stash before changeing all quaternion operations

--- eigenOpenCv/test.cpp
+++ eigenOpenCv/test.cpp
@@ -12,16 +12,17 @@
 
 int main( int argc, char** argv )
 {
-
+	/*
 	QuaternionAlias<double> q_a1( 1, 0, 0, 0 );
 	QuaternionAlias<double> q_a2 = QuaternionAlias<double>( 1, 0, 0, 0 );
-	Quaternion<double> q1 = Quaternion<double>( 1, 0, 0, 1 );
+	Quaternion<double> q1 = Quaternion<double>( 0, 0, 0, 1 );
 	Quaternion<double> q2 = Quaternion<double>( 1, 0, 0, 0 );
 
 	std::cout << "q_a1: " << q_a1.coeffs() << " q_a2: " << q_a2.coeffs() << std::endl;
 	std::cout << "q_a1 * q_a2: " << (q_a1*q_a2).coeffs() << std::endl;
 	std::cout << "q1: " << q1.coeffs() << " q2: " << q2.coeffs() << std::endl;
 	std::cout << "q1 * q2: " << (q1*q2).coeffs() << std::endl;
+	*/
 
 	Calib calib;
 	calib.o_x = 300.8859;
