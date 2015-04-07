0e005a2806e4226f005951de66a9a2d6514d467b
Skrogh <s123369@student.dtu.dk>
2015-04-07 15:54:14 +0200

    added alias quaternion def.

--- eigenOpenCv/quaternionAlias.hpp
+++ eigenOpenCv/quaternionAlias.hpp
@@ -36,7 +36,7 @@ public:
 	typedef typename internal::traits<QuaternionAlias>::Coefficients Coefficients;
 	typedef typename Base::AngleAxisType AngleAxisType;
 
-	QuaternionAlias : Quaternion<Scalar, _Options>();
+	inline QuaternionAlias : Quaternion<Scalar, _Options>();
 /*
 	inline QuaternionAlias : Quaternion<_Scalar,_Options>() {}
 
