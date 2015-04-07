47c85f1ff23d736d5480bbe0d97dd942d57df090
Skrogh <s123369@student.dtu.dk>
2015-04-07 14:36:11 +0200

    added alias quaternion def.

--- eigenOpenCv/quaternionAlias.hpp
+++ eigenOpenCv/quaternionAlias.hpp
@@ -29,15 +29,9 @@ class QuaternionAlias : public Quaternion<_Scalar,_Options>
 	enum { IsAligned = internal::traits<QuaternionAlias>::IsAligned };
 
 public:
+	typedef _Scalar Scalar;
 
-	template<class OtherDerived> EIGEN_STRONG_INLINE QuaternionAlias<Scalar> operator* (const QuaternionAlias<OtherDerived>& q) const
-	{
-		EIGEN_STATIC_ASSERT((internal::is_same<typename Derived::Scalar, typename OtherDerived::Scalar>::value),
-		YOU_MIXED_DIFFERENT_NUMERIC_TYPES__YOU_NEED_TO_USE_THE_CAST_METHOD_OF_MATRIXBASE_TO_CAST_NUMERIC_TYPES_EXPLICITLY)
-		return internal::quat_product<Architecture::Target, Derived, OtherDerived,
-				typename internal::traits<Derived>::Scalar,
-				internal::traits<Derived>::IsAligned && internal::traits<OtherDerived>::IsAligned>::run(other, *this);
-	}
+	template<class OtherDerived> EIGEN_STRONG_INLINE QuaternionAlias<Scalar> operator* (const QuaternionAlias<OtherDerived>& q) const;
 	/*
 	//template<class OtherDerived> EIGEN_STRONG_INLINE Derived& operator*= (const QuaternionBase<OtherDerived>& q) 
 	Matrix3 toRotationMatrix() const 
@@ -50,6 +44,19 @@ public:
 	}
 */
 };
+
+template <class Derived>
+template <class OtherDerived>
+EIGEN_STRONG_INLINE QuaternionAlias<typename internal::traits<Derived>::Scalar>
+QuaternionAlias<Derived>::operator* (const QuaternionAlias<OtherDerived>& other) const
+{
+	EIGEN_STATIC_ASSERT((internal::is_same<typename Derived::Scalar, typename OtherDerived::Scalar>::value),
+	YOU_MIXED_DIFFERENT_NUMERIC_TYPES__YOU_NEED_TO_USE_THE_CAST_METHOD_OF_MATRIXBASE_TO_CAST_NUMERIC_TYPES_EXPLICITLY)
+
+	return internal::quat_product<Architecture::Target, Derived, OtherDerived,
+			typename internal::traits<Derived>::Scalar,
+			internal::traits<Derived>::IsAligned && internal::traits<OtherDerived>::IsAligned>::run(other, *this);
+}
 
 }
 #endif//_QUATERNION_ALIAS_H_
