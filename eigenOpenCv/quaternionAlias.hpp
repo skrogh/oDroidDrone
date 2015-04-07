1e2462a36f0b340fb9f11aff0d680fc0c23f923d
Skrogh <s123369@student.dtu.dk>
2015-04-07 13:29:42 +0200

    added alias quaternion def.

--- eigenOpenCv/quaternionAlias.hpp
+++ eigenOpenCv/quaternionAlias.hpp
@@ -11,9 +11,10 @@ class QuaternionAlias : public Quaternion<_Scalar,_Options>
 
 	typedef Quaternion<_Scalar,_Options> Base;
 	enum { IsAligned = internal::traits<QuaternionAlias>::IsAligned };
-	/*
+
 public:
-	template<class OtherDerived> EIGEN_STRONG_INLINE QuaternionAlias<Scalar> operator* (const QuaternionAlias<OtherDerived>& q) const
+	template<class OtherDerived> EIGEN_STRONG_INLINE QuaternionAlias<Scalar> operator* (const QuaternionAlias<OtherDerived>& q) const;
+/*
 	{
 		EIGEN_STATIC_ASSERT((internal::is_same<typename Derived::Scalar, typename OtherDerived::Scalar>::value),
 		YOU_MIXED_DIFFERENT_NUMERIC_TYPES__YOU_NEED_TO_USE_THE_CAST_METHOD_OF_MATRIXBASE_TO_CAST_NUMERIC_TYPES_EXPLICITLY)
