c8a1fb2bdc2214e886273a2036a83881d33b4cff
Skrogh <s123369@student.dtu.dk>
2015-04-09 20:14:52 +0200

    increase unity test precission

--- eigenOpenCv/printMatlabVarAsEigen.m
+++ eigenOpenCv/printMatlabVarAsEigen.m
@@ -6,14 +6,14 @@ if ~isempty(typeEigen)
     fprintf( file, '%s %s(%d, %d);\n', typeEigen, nameEigen, size(varMatlab,1), size(varMatlab,2) );
 end
 fprintf( file, '%s <<\n', nameEigen );
-fprintf( file, '%e', varMatlab_(1) );
+fprintf( file, '%.16e', varMatlab_(1) );
 for i=2:length( varMatlab(:) )
     if mod(i-1,size(varMatlab,2)) ~= 0
         fprintf( file, ',\t');
     else
         fprintf( file, ',\n');
     end
-    fprintf( file, '%e', varMatlab_(i) );
+    fprintf( file, '%.16e', varMatlab_(i) );
 end
 
 fprintf( file, ';\n' );
