60a736f46d6104644bb3cca5962fe35d9c096778
Skrogh <s123369@student.dtu.dk>
2015-04-10 17:30:18 +0200

    begin making feature detection faster

--- eigenOpenCv/opencvTest.cpp
+++ eigenOpenCv/opencvTest.cpp
@@ -19,10 +19,10 @@ int main( int argc, char** argv )
 	int key = 0;
 	while( ( key = cv::waitKey(1) ) != 27 ) {
 		cap.grab();
+/*		cap.grab();
 		cap.grab();
 		cap.grab();
-		cap.grab();
-		cap.grab();
+		cap.grab();*/
 		cap.retrieve( image );
 		cvtColor(image, image, CV_BGR2GRAY);
 
