0d62007408d2eda3833066578d638282177ea07a
Skrogh <s123369@student.dtu.dk>
2015-04-13 21:53:45 +0200

    debug kalman gain calculation

--- eigenOpenCv/odometry.cpp
+++ eigenOpenCv/odometry.cpp
@@ -706,7 +706,7 @@ void MSCKF::updateCamera( CameraMeasurements &cameraMeasurements ) {
 		// Kalman gain
 		MatrixXd K = sigma * H0.transpose() * ( H0 * sigma * H0.transpose() + R_q ).inverse();
 		std::cout << "Kalman gain debug: \n" << ( H0 * sigma * H0.transpose() + R_q ) << "\n" <<
-		( H0 * sigma * H0.transpose() + R_q ).inverse() << "\n"
+		( H0 * sigma * H0.transpose() + R_q ).inverse() << "\n" <<
 		( ( H0 * sigma * H0.transpose() + R_q ) * ( ( H0 * sigma * H0.transpose() + R_q ).inverse() ) ) << std::endl;
 
 		//std::cout << "r0: " << r0 << std::endl;
