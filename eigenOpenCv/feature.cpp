d0f8fb970567768040aa2dac99893a1eb955f004
Skrogh <s123369@student.dtu.dk>
2015-04-13 09:57:33 +0200

    add timing to tracking test

--- eigenOpenCv/feature.cpp
+++ eigenOpenCv/feature.cpp
@@ -8,7 +8,7 @@
 using namespace cv;
 
 CameraDetector::CameraDetector( void ) {
-	int maxCorners = 500;
+	int maxCorners = 100;
 	double qualityLevel = 0.02;
 	double minDistance = 5;
 	int blockSize = 3;
