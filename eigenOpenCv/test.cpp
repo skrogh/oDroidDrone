72dac5a4ed637235cd67d0d77ad34714aa555897
Skrogh <s123369@student.dtu.dk>
2015-04-13 12:37:39 +0200

    read from file

--- eigenOpenCv/test.cpp
+++ eigenOpenCv/test.cpp
@@ -13,7 +13,7 @@ void getImageFromFile( cv::Mat& image, timeval& tv, std::istream& fileOfFiles )
 {
 	std::string line;
 	if ( std::getline( fileOfFiles, line ) ) {
-		sscanf( line.c_str(), "img-s%d.%d.png", &(tv.tv_sec), &(tv.tv_usec) );
+		sscanf( line.c_str(), "%*[^i]%*[^m]%*[^g]%*[^-]%*[^s]s%d.%d.png", &(tv.tv_sec), &(tv.tv_usec) );
 		image = imread( line );
 	}
 }
