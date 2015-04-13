b51655bad64dd44d12e38934acbaeffa66f0b010
Skrogh <s123369@student.dtu.dk>
2015-04-13 13:02:16 +0200

    remove initial calibration

--- eigenOpenCv/common.cpp
+++ eigenOpenCv/common.cpp
@@ -20,7 +20,7 @@ void CameraMeasurements::addFeatures( const std::vector<cv::KeyPoint> &keypoints
 	link.assign( keypointsNew.size(), meas.end() );
 
 	// Go through all features with a match, add them at the correct place
-	for ( int i = 0; i < matches.size(); i++ ) if ( matches[i].distance < 100 ) {
+	for ( int i = 0; i < matches.size(); i++ ) if ( matches[i].distance < 30 ) {
 		// Check if this feature is in meas
 		if ( linkOld[matches[i].queryIdx] != meas.end() ) {
 			//
