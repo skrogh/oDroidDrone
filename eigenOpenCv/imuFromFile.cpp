b82b06edfa32bb215aab423d70a48e2b35f8367d
Skrogh <s123369@student.dtu.dk>
2015-04-13 11:38:09 +0200

    read from file

--- eigenOpenCv/imuFromFile.cpp
+++ eigenOpenCv/imuFromFile.cpp
@@ -13,7 +13,7 @@ Imu::Imu( char* fileName ) {
 			&(element.acc[0]), &(element.acc[1]), &(element.acc[2]),
 			&(element.gyro[0]), &(element.gyro[1]), &(element.gyro[2]),
 			&(element.alpha[0]), &(element.alpha[1]), &(element.alpha[2]),
-			&dist
+			&ping
 		);
 		element.distValid = (ping != 0);
 		element.dist = ping * ( 340.29 / 80000000.0 ) / 2.0 - 0.0565;
@@ -26,38 +26,28 @@ Imu::~Imu( ) {
 }
 
 void Imu::fifoPush( const ImuMeas_t &element ) {
-	pthread_mutex_lock( &fifoMutex );
 	dataFifo.push_back( element );
-	pthread_mutex_unlock( &fifoMutex );
 }
 
 bool Imu::fifoPop( ImuMeas_t &element ) {
-	pthread_mutex_lock( &fifoMutex );
 	if( !dataFifo.empty() ) {
 		element = dataFifo.front( );
 		dataFifo.pop_front( );
-		pthread_mutex_unlock( &fifoMutex );
 		return true;
 	}
-	pthread_mutex_unlock( &fifoMutex );
 	return false;
 }
 
 bool Imu::fifoPeak( unsigned int n, ImuMeas_t &element ) {
-	pthread_mutex_unlock( &fifoMutex );
 	if( dataFifo.size() > n ) {
 		element = dataFifo.at( n );
-		pthread_mutex_unlock( &fifoMutex );
 		return true;
 	}
-	pthread_mutex_unlock( &fifoMutex );
 	return false;
 }
 
 
 unsigned int Imu::fifoSize( void ) {
-	pthread_mutex_unlock( &fifoMutex );
 	unsigned int size = dataFifo.size();
-	pthread_mutex_unlock( &fifoMutex );
 	return size;
 }
