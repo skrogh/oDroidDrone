d7b6ea8b6d7658f1b57ec6998fc7b80ff333db43
Skrogh <s123369@student.dtu.dk>
2015-04-15 13:57:12 +0200

    lkdemo update

--- eigenOpenCv/lkdemo.cpp
+++ eigenOpenCv/lkdemo.cpp
@@ -43,7 +43,7 @@ int main( int argc, char** argv )
     TermCriteria termcrit(TermCriteria::COUNT|TermCriteria::EPS,20,0.03);
     Size subPixWinSize(10,10), winSize(31,31);
 
-    const int MAX_COUNT = 500;
+    const int MAX_COUNT = 200;
     bool needToInit = false;
     bool nightMode = false;
 
@@ -84,7 +84,7 @@ int main( int argc, char** argv )
         {
             // automatic initialization
             goodFeaturesToTrack(gray, points[1], MAX_COUNT, 0.01, 10, Mat(), 3, 0, 0.04);
-            cornerSubPix(gray, points[1], subPixWinSize, Size(-1,-1), termcrit);
+            //cornerSubPix(gray, points[1], subPixWinSize, Size(-1,-1), termcrit);
             addRemovePt = false;
         }
         else if( !points[0].empty() )
@@ -120,7 +120,7 @@ int main( int argc, char** argv )
         {
             vector<Point2f> tmp;
             tmp.push_back(point);
-            cornerSubPix( gray, tmp, winSize, Size(-1,-1), termcrit);
+            //cornerSubPix( gray, tmp, winSize, Size(-1,-1), termcrit);
             points[1].push_back(tmp[0]);
             addRemovePt = false;
         }
