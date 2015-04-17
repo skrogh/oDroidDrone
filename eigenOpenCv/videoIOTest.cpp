3e409b4d9b9049081f857d11bdbefdf58d3423a4
Skrogh <s123369@student.dtu.dk>
2015-04-17 17:15:24 +0200

    using video server

---
+++ eigenOpenCv/videoIOTest.cpp
@@ -0,0 +1,15 @@
+#include "videoIO.hpp"
+
+int main( int argc, char** argv )
+{
+	VideoIn videoIn( 0 );
+	cv::Mat frame;
+
+	namedWindow( "Features", 1 );
+
+	imshow("Features", frame);
+
+	char c = (char)waitKey(1);
+	if( c == 27 )
+		break;
+}
