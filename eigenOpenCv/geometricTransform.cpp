695fd3c893b647ceb81e9c40376b4596921bf21f
Skrogh <s123369@student.dtu.dk>
2015-05-13 15:14:59 +0200

    added ransac

--- eigenOpenCv/geometricTransform.cpp
+++ eigenOpenCv/geometricTransform.cpp
@@ -1,26 +1,10 @@
 #include "geometricTransform.hpp"
+#include <random>
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
+#include <cmath>
 
 using namespace Eigen;
-
-Matrix<double,4,1> computeSimilarityOld(
-    const Matrix2Xd& pointsFrom, const Matrix2Xd& pointsTo ) {
-  Matrix<double, Dynamic, 5> C( pointsTo.cols()*2, 5 );
-  for ( int i = 0; i < pointsTo.cols(); i++ ) {
-    const double &x = pointsTo(0,i);
-    const double &y = pointsTo(1,i);
-    const double &x_ = pointsFrom(0,i);
-    const double &y_ = pointsFrom(1,i);
-    C.row( i*2 )    << -y,  x,  0, -1,  y_;
-    C.row( i*2 + 1) <<  x,  y,  1,  0, -x_;
-  }
-  // solve for transformation
-  JacobiSVD<MatrixXd> svd( C, ComputeThinV );
-  VectorXd V = svd.matrixV().rightCols<1>();
-  // find transformation
-  Matrix<double,4,1> h = V.head<4>() / V(4);
-
-  return h;
-}
 
 Matrix3d computeSimilarity(
     const Matrix2Xd& pointsFrom, const Matrix2Xd& pointsTo ) {
@@ -104,6 +88,84 @@ Matrix3d computeTform(
   return normMatFrom.transpose() * ( T * normMatTo.transpose().inverse() );
 }
 
+VectorXd evaluateTform( const Matrix2Xd& pointsFrom, const Matrix2Xd& pointsTo,
+    const Matrix3d& T, double threshold ) {
+  Matrix2Xd pt( pointsFrom.rows(), pointsFrom.cols() );
+  pt = T.block<2,2>(0,0).transpose() * pointsFrom;
+  for( int i = 0; i < pointsFrom.cols(); i++ ) {
+    pointsFrom.col(i) += T.block<2,1>(0,2);
+  }
+
+  pt = pt - pointsTo;
+  VectorXd dis( pt.cols() );
+  for ( int i = 0; i < dis.rows(); i++ ) {
+    dis(i) = pt.col(i).norm();
+    if( dis(i) > threshold )
+      dis(i) = threshold;
+  }
+
+  return dis;
+}
+
+std::default_random_engine msacGenerator;
+Matrix3d msac( const Eigen::Matrix2Xd& pointsFrom, const Eigen::Matrix2Xd& pointsTo,
+    int maxNumTrials, double confidence, double maxDistance ) {
+  double threshold = maxDistance;
+  int numPts = pointsFrom.cols();
+  int idxTrial = 1;
+  int numTrials = maxNumTrials;
+  double maxDis = threshold * numPts;
+  double bestDist = maxDis;
+  Matrix3d bestT;
+  bestT << 1, 0, 0,   0, 1, 0,  0, 0, 1;
+
+  int index1;
+  int index2;
+
+  while ( idxTrial <= numTrials ) {
+    // Get two random, different numbers in [0:pointsFrom.cols()-1]
+    std::uniform_int_distribution<int> distribution1( 0, pointsFrom.cols()-1 );
+    index1 = distribution1( msacGenerator );
+    std::uniform_int_distribution<int> distribution2( 0, pointsFrom.cols()-2 );
+    index2 = distribution2( msacGenerator );
+    if ( index2 >= index1 )
+      index2++;
+    Vector2d indices( index1, index2 );
+
+    // Get T form Calculated from this set of points
+    Matrix3d T = computeTform( pointsFrom, pointsTo, indices );
+
+    VectorXd dis = evaluateTform( pointsFrom, pointsTo, T, threshold );
+
+    double accDis = dis.sum();
+
+    if ( accDis < bestDist ) {
+      bestDist = accDis;
+      bestT = T;
+    }
+    numTrials++;
+  }
+  VectorXd dis = evaluateTform( pointsFrom, pointsTo, bestT, threshold );
+  int numInliers = 0;
+  for ( int i = 0; i < dis.rows(); i++ ){
+    if ( dis(i) < threshold )
+      numInliers++;
+  }
+  VectorXd inliers( numInliers );
+  int j = 0;
+  for ( int i = 0; i < dis.rows(); i++ ){
+    if ( dis(i) < threshold )
+      inliers(j++) = i;
+  }
+
+  Matrix3d T;
+  if ( numInliers >= 2 )
+    T = computeTform( pointsFrom, pointsTo, inliers );
+  else
+    T << 1, 0, 0,  0, 1, 0,  0, 0, 1;
+
+  return T;
+}
 
 Eigen::Matrix<double,4,1> estimateSimilarTransform(
     const Eigen::Matrix2Xd& pointsFrom, const Eigen::Matrix2Xd& pointsTo ) {
@@ -112,7 +174,7 @@ Eigen::Matrix<double,4,1> estimateSimilarTransform(
   for ( int i = 0; i < indices.rows(); i++ ) {
     indices(i) = i;
   }
-  Matrix3d T = computeSimilarity( pointsFrom, pointsTo );
+  Matrix3d T = msac( pointsFrom, pointsTo, 20, 99, 2 );
 
   h(0) = T(0,0);
   h(1) = T(1,0);
