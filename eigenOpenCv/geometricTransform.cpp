ebfb0d440244614f22145de64d59d5bcb4f340e7
Skrogh <s123369@student.dtu.dk>
2015-05-13 13:30:06 +0200

    typos

--- eigenOpenCv/geometricTransform.cpp
+++ eigenOpenCv/geometricTransform.cpp
@@ -2,6 +2,30 @@
 
 using namespace Eigen;
 
+Matrix3d computeSimilarity(
+    const Matrix2Xd& pointsFrom, const Matrix2Xd& pointsTo ) {
+  Matrix<double, Dynamic, 5> C( pointsTo.cols()*2, 5 );
+  for ( int i = 0; i < pointsTo.cols(); i++ ) {
+    const double &x = pointsTo(0,i);
+    const double &y = pointsTo(1,i);
+    const double &x_ = pointsFrom(0,i);
+    const double &y_ = pointsFrom(1,i);
+    C.row( i*2 )    << -y,  x,  0, -1,  y_;
+    C.row( i*2 + 1) <<  x,  y,  1,  0, -x_;
+  }
+  // solve for transformation
+  JacobiSVD<MatrixXd> svd( C, ComputeThinV );
+  VectorXd V = svd.matrixV().rightCols<1>();
+  // find transformation
+  Matrix<double,4,1> h = V.head<4>() / V(4);
+
+  Matrix3d T;
+  T << h(0), -h(1), 0,
+       h(1),  h(0), 0,
+       h(2),  h(3), 1;
+  return T;
+}
+
 Matrix2Xd normalizePoints( const Matrix2Xd& points, const VectorXd& indices,
     Ref<Matrix3d> T ) {
   Matrix2Xd pointsOut( 2, indices.rows() );
@@ -57,30 +81,6 @@ Matrix3d computeTform(
   return normMatFrom.transpose() * ( T * normMatTo.transpose().inverse() );
 }
 
-
-Matrix3d computeSimilarity(
-    const Matrix2Xd& pointsFrom, const Matrix2Xd& pointsTo ) {
-  Matrix<double, Dynamic, 5> C( pointsTo.cols()*2, 5 );
-  for ( int i = 0; i < pointsTo.cols(); i++ ) {
-    const double &x = pointsTo(0,i);
-    const double &y = pointsTo(1,i);
-    const double &x_ = pointsFrom(0,i);
-    const double &y_ = pointsFrom(1,i);
-    C.row( i*2 )    << -y,  x,  0, -1,  y_;
-    C.row( i*2 + 1) <<  x,  y,  1,  0, -x_;
-  }
-  // solve for transformation
-  JacobiSVD<MatrixXd> svd( C, ComputeThinV );
-  VectorXd V = svd.matrixV().rightCols<1>();
-  // find transformation
-  Matrix<double,4,1> h = V.head<4>() / V(4);
-
-  Matrix3d T;
-  T << h(0), -h(1), 0,
-       h(1),  h(0), 0,
-       h(2),  h(3), 1;
-  return T;
-}
 
 Eigen::Matrix<double,4,1> estimateSimilarTransform(
     const Eigen::Matrix2Xd& pointsFrom, const Eigen::Matrix2Xd& pointsTo ) {
