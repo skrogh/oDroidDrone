e1c1e557f809a67c03d334ed8f4b763952cd9097
Skrogh <s123369@student.dtu.dk>
2015-06-12 15:09:23 +0200

    les agressive controller

--- eigenOpenCv/mserver.m
+++ eigenOpenCv/mserver.m
@@ -1,5 +1,5 @@
 %% Client for recieving data from drone.
-tcpipClient = udp('10.16.173.52',55000);
+tcpipClient = udp('10.16.165.94',55000);
 set(tcpipClient, 'inputbuffersize', 2^15 )
 fclose(tcpipClient)
 fopen(tcpipClient)                          % connect
