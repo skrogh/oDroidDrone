1113d9443664976f67122aee93d367957160ccbd
Søren Andersen  (emb.) <s123369@stuent.dtu.dk>
2015-06-22 20:24:35 +0930

    added path script

---
+++ eigenOpenCv/patterns/line.sh
@@ -0,0 +1,23 @@
+sleep 10
+echo 'P: 0, 0, 400'
+sleep 5
+echo 'P: 500, 0, 400'
+sleep 6
+echo 'P: 0, 0, 400'
+sleep 6
+echo 'P: 500, 0, 400'
+sleep 6
+echo 'P: 0, 0, 400'
+sleep 6
+echo 'P: 500, 0, 400'
+sleep 6
+echo 'P: 0, 0, 400'
+sleep 6
+echo 'P: 500, 0, 400'
+sleep 6
+echo 'P: 0, 0, 400'
+sleep 6
+echo 'P: 0, 0, 200'
+sleep 3
+echo 'P: 0, 0, 1'
+sleep 100h
