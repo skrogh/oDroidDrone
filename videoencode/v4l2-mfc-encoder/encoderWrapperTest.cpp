f06ea63be3824835f85a73f7644761f003e3ef0d
Skrogh <s123369@student.dtu.dk>
2015-08-16 00:44:57 +0200

    cleaning up interface, makeing it less comanline-ish and more program ish

--- videoencode/v4l2-mfc-encoder/encoderWrapperTest.cpp
+++ videoencode/v4l2-mfc-encoder/encoderWrapperTest.cpp
@@ -43,7 +43,7 @@ int main(int argc, char *argv[])
   int from_to[] = { 0,0, 2,1, 1,2 };
   cv::mixChannels(&YcrCb, 1, out, 2, from_to, 3);
 
-  cv::resize(CbCr, CbCr_2, cv::Size(), 0.5, 0.5, cV::INTER_NEAREST);
+  cv::resize(CbCr, CbCr_2, cv::Size(), 0.5, 0.5, cv::INTER_NEAREST);
   inBuff[0] = (char*) Gray.data;
   inBuff[1] = (char*) CbCr_2.data;
 
