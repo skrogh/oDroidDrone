e521f96a3ee45d2880484c7263cdc697274bf701
Skrogh <s123369@student.dtu.dk>
2015-08-15 23:19:18 +0200

    cleaning up interface, makeing it less comanline-ish and more program ish

--- videoencode/v4l2-mfc-encoder/in_demo.h
+++ videoencode/v4l2-mfc-encoder/in_demo.h
@@ -25,6 +25,6 @@
 
 #include "io_dev.h"
 
-struct io_dev *in_demo_create(int width, int height, int encoderFd);
+struct io_dev *in_demo_create(int width, int height, int encoderFd, char **NU12_ARRAY);
 
 #endif
