0cbc2b448eca9b354cc3d45f24f0e7674270670b
Skrogh <s123369@student.dtu.dk>
2015-08-15 16:38:00 +0200

    try with polling on stdin

--- videoencode/v4l2-mfc-encoder/in_demo.c
+++ videoencode/v4l2-mfc-encoder/in_demo.c
@@ -104,7 +104,7 @@ struct io_dev *in_demo_create(int width, int height)
 	priv->width = width;
 	priv->height = height;
 
-	dev->fd = STDIN;
+	dev->fd = stdin;
 	dev->io[DIR_IN].type = IO_NONE;
 	dev->io[DIR_OUT].type = IO_FUNC;
 	dev->ops = &in_demo_ops;
