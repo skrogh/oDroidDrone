efd7b7e90b9bc1be0eaea2262ad55af572bd72b2
Skrogh <s123369@student.dtu.dk>
2015-08-15 22:50:03 +0200

    begin making c++ wrapper

--- videoencode/v4l2-mfc-encoder/encoderWrapperTest.cpp
+++ videoencode/v4l2-mfc-encoder/encoderWrapperTest.cpp
@@ -9,11 +9,11 @@ int main(int argc, char *argv[])
 {
 	struct options opts;
 
-	std::cout << "mfc codec encoding example application" << std:endl
-	       "Original by Andrzej Hajda <a.hajda@samsung.com>" << std:endl
-	       "Copyright 2012 Samsung Electronics Co., Ltd." << std:endl
-         "openCV interface by Søren Andersen." << std:endl
-         "Copyright 2015" << std:endl << std:endl;
+	std::cout << "mfc codec encoding example application" << std::endl
+	       "Original by Andrzej Hajda <a.hajda@samsung.com>" << std::endl
+	       "Copyright 2012 Samsung Electronics Co., Ltd." << std::endl
+         "openCV interface by Søren Andersen." << std::endl
+         "Copyright 2015" << std::endl << std::endl;
 
 	if (parse_args(&opts, argc, argv)) {
 		print_usage(argv[0]);
